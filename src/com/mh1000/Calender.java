fbf92c8f8140b87c79d1f97abc1ea933ca220fd4
HOON_BOOK10.6 <HOON_BOOK10.6@HOON-BOOK>
2018-08-02 19:57:37 +0900

    method 추가

--- src/com/mh1000/Calender.java
+++ src/com/mh1000/Calender.java
@@ -3,20 +3,21 @@ package com.mh1000;
 import java.util.Scanner;
 
 public class Calender {
+	
+	private static final int[] MAX_DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
+	public int getMaxDays(int a) {
+		return MAX_DAYS[a-1];
+	}
+	
 	public static void main(String[] args) {
 		// 숫자 입력 받아 해당 달의 최대 일수 출력
 		
-	
-		int[] month = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-		
 		Scanner mon = new Scanner(System.in);
-		
+		Calender cal = new Calender();
 		System.out.println("궁금한 월을 입력하시오");
-		
 		int a = mon.nextInt();
-		
-		System.out.println("입력한 월의 마지막 일은"+ month[a-1] + "입니다.");
+		System.out.printf("%d월은 %d일까지 있습니다.", a, cal.getMaxDays(a));
 		
 				
 	}
