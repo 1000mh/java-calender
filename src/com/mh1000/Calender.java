f9f9ad401903f6cd35beb4c4028002247baf45fc
HOON_BOOK10.6 <HOON_BOOK10.6@HOON-BOOK>
2018-08-03 14:13:46 +0900

    종료, 반복 적용

--- src/com/mh1000/Calender.java
+++ src/com/mh1000/Calender.java
@@ -13,24 +13,31 @@ public class Calender {
 	public static void main(String[] args) {
 		// 숫자 입력 받아 해당 달의 최대 일수 출력
 		
-		Scanner userIn = new Scanner(System.in);
-		System.out.println("반복횟수를 입력하시오");
-		int i = userIn.nextInt();
+		String PROMPT="cal> ";
 		
-		for (int j=0; j<i; j++) {
-			Scanner mon = new Scanner(System.in);
+		Scanner mon = new Scanner(System.in);
+		int a = 0;
+		
+		while (true) {
 			System.out.println("궁금한 월을 입력하시오");
-			int a = mon.nextInt();
+			System.out.print(PROMPT);
+			a = mon.nextInt();
+			
+			if(a < 0) {
+				System.out.println("-1 입력하여 종료합니다.");
+				break;
+			}
 
+			if(a > 12) {
+				System.out.println("입력값을 초과하였습니다.");
+				continue;
+			}
+			
 			Calender cal = new Calender();
 			System.out.printf("%d월은 %d일까지 있습니다.\n", a, cal.getMaxDays(a));	
 		}
-	
 		System.out.println("끝");
-		
-		
-		
-				
+
 	}
 
 }
