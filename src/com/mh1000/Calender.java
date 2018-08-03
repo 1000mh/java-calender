69c541d8b15862c0774102a51c0e97342f57b329
HOON_BOOK10.6 <HOON_BOOK10.6@HOON-BOOK>
2018-08-03 13:43:30 +0900

    반복월 입력 후 출력

--- src/com/mh1000/Calender.java
+++ src/com/mh1000/Calender.java
@@ -13,11 +13,22 @@ public class Calender {
 	public static void main(String[] args) {
 		// 숫자 입력 받아 해당 달의 최대 일수 출력
 		
-		Scanner mon = new Scanner(System.in);
-		Calender cal = new Calender();
-		System.out.println("궁금한 월을 입력하시오");
-		int a = mon.nextInt();
-		System.out.printf("%d월은 %d일까지 있습니다.", a, cal.getMaxDays(a));
+		Scanner userIn = new Scanner(System.in);
+		System.out.println("반복횟수를 입력하시오");
+		int i = userIn.nextInt();
+		
+		for (int j=0; j<i; j++) {
+			Scanner mon = new Scanner(System.in);
+			System.out.println("궁금한 월을 입력하시오");
+			int a = mon.nextInt();
+
+			Calender cal = new Calender();
+			System.out.printf("%d월은 %d일까지 있습니다.\n", a, cal.getMaxDays(a));	
+		}
+		
+			
+		
+		
 		
 				
 	}
